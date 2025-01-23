9434972ec90e83618aa2c1ae20e3296f7188a38f
Sergio Guzman <serguz95444@gmail.com>
2025-01-23 16:15:32 -0600

    Actualizar el diseño de la interfaz

--- src/index.js
+++ src/index.js
@@ -1,89 +1,100 @@
-let price = 1.87;
-let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+const price = 1.87;
+const cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
 ];
-const changeDue = document.getElementById("change-due");
+document.getElementById("value").textContent = `${price}`;
+const $status = document.getElementById("notice-status");
+const $changeDue = document.getElementById("change-due");
 
+const updateStatus = (type) => {
+  $status.classList.remove("error", "warning", "success");
 
+  switch (type) {
+  case "error":
+    $status.classList.add("error");
+    $status.textContent = "Status: CLOSED";
+    break;
+
+  case "warning":
+    $status.classList.add("warning");
+    $status.textContent = "Status: INSUFFICIENT_FUNDS";
+    break;
+
+  case "success":
+    $status.classList.add("success");
+    $status.textContent = "Status: OPEN";
+    break;
+
+  default:
+    break;
+  }
+};
 
 const calculateChange = cash => {
-  //sacar el cambio
+  // sacar el cambio
   let change = cash - price;
-  console.log(change);
-  
 
   const denominations = {
-    "PENNY": 0.01,
-    "NICKEL": 0.05,
-    "DIME": 0.10,
-    "QUARTER": 0.25,
-    "ONE": 1.00,
-    "FIVE": 5.00,
-    "TEN": 10.00,
-    "TWENTY": 20.00,
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.10,
+    QUARTER: 0.25,
+    ONE: 1.00,
+    FIVE: 5.00,
+    TEN: 10.00,
+    TWENTY: 20.00,
     "ONE HUNDRED": 100.00
   };
 
+  const changeReturn = [];
 
-  let changeReturn = [];
-  
-    for (let i = cid.length - 1; i >= 0; i--) {
-        let [denomination, quantityAvailable] = cid[i];
-        let valueDenomination = denominations[denomination];
-        let amountNecessary = Math.floor(change / valueDenomination);
+  for (let i = cid.length - 1; i >= 0; i--) {
+    const [denomination, quantityAvailable] = cid[i];
+    const valueDenomination = denominations[denomination];
+    const amountNecessary = Math.floor(change / valueDenomination);
 
-        if (quantityAvailable > 0 && amountNecessary > 0) {
-          console.log("denominacion:",denomination);
-          console.log("cantidadDisponible:",quantityAvailable );
-          console.log("cantidadNecesaria:",amountNecessary);
-          
-          let quantityReturn = Math.min(amountNecessary, Math.floor(quantityAvailable / valueDenomination));
-          console.log("quantityReturn:", quantityReturn);
-          
-          
-           let quantityReturned = (quantityReturn * valueDenomination).toFixed(2);
-          console.log("cantidad devuelta:", quantityReturned);
-          
-          
+    if (quantityAvailable > 0 && amountNecessary > 0) {
+      const quantityReturn = Math.min(amountNecessary, Math.floor(quantityAvailable / valueDenomination));
 
-          if (quantityReturned > 0 && quantityAvailable >= quantityReturned) {
-              changeReturn.push([denomination, parseFloat(quantityReturned)]);
-              change = parseFloat((change - parseFloat(quantityReturned)).toFixed(2));  // Restar del change
-              cid[i][1] = parseFloat((cid[i][1] - parseFloat(quantityReturned)).toFixed(2));  // Restar de la caja
-              const allZero = cid.every(([coin, value]) => value === 0);
-              console.log(allZero);
-              
-              if (allZero && change === 0) {
-              changeDue.textContent =  `Status: CLOSED ${changeReturn.map(item => `${item[0]}: $${item[1]}`).join(" ")}`;
-                return;
-              }
-          }
-          
-          console.log("cambio: ",change);
+      const quantityReturned = (quantityReturn * valueDenomination).toFixed(2);
 
+      if (quantityReturned > 0 && quantityAvailable >= quantityReturned) {
+        changeReturn.push([denomination, parseFloat(quantityReturned)]);
+        change = parseFloat((change - parseFloat(quantityReturned)).toFixed(2)); // Restar del change
+        cid[i][1] = parseFloat((cid[i][1] - parseFloat(quantityReturned)).toFixed(2)); // Restar de la caja
+        const allZero = cid.every(([coin, value]) => value === 0);
+
+        if (allZero && change === 0) {
+          updateStatus("error");
+		  $changeDue.textContent = ` ${changeReturn.map(item => `${item[0]}: $${item[1]}`).join(" ")}`;
+          return;
         }
-
-        if (parseFloat(change) <= 0) break;
-        
+      }
     }
 
-    if (change > 0) {
-        changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
-    } else {
-        changeDue.textContent = `Status: OPEN ${changeReturn.map(item => `${item[0]}: $${item[1]}`).join(" ")}`;
-    }
-}
+    if (parseFloat(change) <= 0) break;
+  }
 
+  if (change > 0) {
+    updateStatus("warning");
+  } else {
+    updateStatus("success");
+    $changeDue.textContent = `${changeReturn.map(item => `${item[0]}: $${item[1]}`).join(" ")}`;
+  }
+};
 
-document.getElementById("purchase-btn").addEventListener("click", () => {
+const updateMoneyTray = () => {
+
+};
+document.getElementById("button").addEventListener("click", () => {
   const cashInput = document.getElementById("cash");
 
   const cash = Number(cashInput.value);
@@ -92,13 +103,11 @@ document.getElementById("purchase-btn").addEventListener("click", () => {
   if (cash < price) {
     alert("Customer does not have enough money to purchase the item");
     return;
-  }else if (cash === price) {
-    changeDue.textContent = "No change due - customer paid with exact cash.";
+  } else if (cash === price) {
+    $changeDue.textContent = "No change due - customer paid with exact cash.";
     return;
   } else {
-    calculateChange(cash)
- 
+    calculateChange(cash);
   }
-  cashInput.value = ""
-})
-
+  cashInput.value = "";
+});
